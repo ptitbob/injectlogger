806ee3d316c8868e331e67708715c9709115f13b
ptitbob <github@shipstone.org>
2017-05-20 23:05:41 +0200

    Initialisation

---
+++ src/main/java/org/shistone/logger/InjectloggerApplication.java
@@ -0,0 +1,12 @@
+package org.shistone.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjectloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjectloggerApplication.class, args);
+	}
+}
